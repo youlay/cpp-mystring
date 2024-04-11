bc7268c776f4c72241017e22adc4192e17d2d29a
youlay <youlay.hong@gmail.com>
2024-04-11 19:20:20 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# cpp-mystring" 
+"# stringlentgh" 
---
+++ mystring.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>;
+using namespace std;
+ int main() {
+  string greeting = "Hello Cpp";
+  cout << greeting;
+  cout << greeting.length();
+  return 0;
+}
+
